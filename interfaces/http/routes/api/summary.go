49c486c45af5ec7819efe5f1e5feaf72823ecc57
elliot alderson <elliot14A@192.168.1.7>
2025-05-01 10:52:32 +0530

    feat(summary): add get summary endpoint

--- interfaces/http/routes/api/summary.go
+++ interfaces/http/routes/api/summary.go
@@ -7,26 +7,16 @@ import (
 	"go.uber.org/zap"
 )
 
-// @Summary Get table schemas
-// @Description Get the schemas information for a dataset/table
-// @Tags query
-// @Accept json
-// @Produce json
-// @Param tableName path string true "Name of the dataset/table" example:"sales_data"
-// @Success 200 {object} map[string]interface{} "Schema information"
-// @Failure 400 {object} responses.ErrorResponse "Invalid table name"
-// @Failure 500 {object} responses.ErrorResponse "Internal server error"
-// @Router /v1/api/schemas/{tableName} [get]
-func (h *httpHandler) schemas(ctx *fiber.Ctx) error {
+func (h *httpHandler) summary(ctx *fiber.Ctx) error {
 	tableName := ctx.Params("tableName")
 
-	schema, err := h.driverSvc.GetTableSchema(tableName)
+	summary, err := h.driverSvc.GetDatasetSummary(tableName)
 	if err != nil {
 		h.logger.Error("Error getting table schema", zap.Error(err))
 		if strings.HasPrefix(err.Error(), "DuckDB") {
 			return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
 				"error":   err.Error(),
-				"message": "Dataset not found",
+				"message": "Invalid query",
 				"code":    fiber.StatusNotFound,
 			})
 		}
@@ -38,6 +28,6 @@ func (h *httpHandler) schemas(ctx *fiber.Ctx) error {
 	}
 
 	return ctx.Status(fiber.StatusOK).JSON(fiber.Map{
-		"schema": schema,
+		"summary": summary,
 	})
 }
