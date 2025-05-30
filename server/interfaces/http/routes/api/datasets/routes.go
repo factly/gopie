6b4cd101a1e0ac9854757a3439f2743408aa1376
elliot14A <akshithkatkuri14@gmail.com>
2025-05-30 12:02:57 +0530

    feat: add get by dataset endpoing at /v1/api level

---
+++ server/interfaces/http/routes/api/datasets/routes.go
@@ -0,0 +1,26 @@
+package datasets
+
+import (
+	"github.com/factly/gopie/application/services"
+	"github.com/factly/gopie/domain/pkg/logger"
+	"github.com/gofiber/fiber/v2"
+)
+
+type httpHandler struct {
+	datasetsSvc *services.DatasetService
+	logger      *logger.Logger
+}
+
+// NewHTTPHandler creates a new HTTP handler for datasets
+func NewHTTPHandler(router fiber.Router, datasetsSvc *services.DatasetService, logger *logger.Logger) {
+	handler := &httpHandler{
+		datasetsSvc: datasetsSvc,
+		logger:      logger,
+	}
+
+	// Setup routes
+	datasetsRouter := router.Group("/datasets")
+	{
+		datasetsRouter.Get("/:datasetID", handler.details)
+	}
+}
