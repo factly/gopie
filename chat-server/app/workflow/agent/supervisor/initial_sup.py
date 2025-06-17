db5b887291faa44f4bf1ba7ea7ac802f3fc67bca
yuvrajsinh5252 <yuvrajsinh476@gmail.com>
2025-06-17 19:40:48 +0000

    feat: add custom handoff tool

--- chat-server/app/workflow/agent/supervisor/initial_sup.py
+++
@@ -1,5 +0,0 @@
-from app.workflow.agent.types import State
-
-
-def route_initial_supervisor(state: State) -> str:
-    return "visualizer_agent"
--- chat-server/app/workflow/agent/supervisor/virtualizer_sup.py
+++
@@ -1,5 +0,0 @@
-from app.workflow.agent.types import State
-
-
-def route_visualization_supervisor(state: State) -> str:
-    return "visualizer_agent"
