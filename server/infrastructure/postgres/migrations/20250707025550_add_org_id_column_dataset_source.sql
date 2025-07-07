02bca1e88b0e3d7bf15d860cd602a93556d5b4a6
elliot14A <akshithkatkuri14@gmail.com>
2025-07-07 11:33:48 +0530

    feat: delete duplicate migration

--- server/infrastructure/postgres/migrations/20250707025550_add_org_id_column_dataset_source.sql
+++
@@ -1,9 +0,0 @@
--- +goose Up
--- +goose StatementBegin
-alter table database_sources add column org_id uuid;
--- +goose StatementEnd
-
--- +goose Down
--- +goose StatementBegin
-alter table database_sources drop column org_id;
--- +goose StatementEnd
