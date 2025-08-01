85e2c5c89918d87cf3133c245a3db38b4ddbaae7
Elliot Alderson <akshithkatkuri14@gmail.com>
2025-08-01 14:55:09 +0530

    feat: set hard limit

--- server/application/services/olap.go
+++ server/application/services/olap.go
@@ -314,6 +314,9 @@ func (d *OlapService) executeDataQuery(sql string, limit, offset int, resultChan
 	var err error
 
 	if imposeLimits {
+		if limit > 1000 {
+			limit = 1000
+		}
 		queryResult, err = d.olap.Query(sql, limitsTransformer(limit, offset))
 	} else {
 		queryResult, err = d.olap.Query(sql)
