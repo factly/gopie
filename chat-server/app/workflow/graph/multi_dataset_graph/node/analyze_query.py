3ef34b2811c668c6972164b0595275a8fda3de2f
yuvrajsinh5252 <yuvrajsinh476@gmail.com>
2025-08-19 05:21:29 +0000

    fix: issues in list_dataset tool and readded it

--- chat-server/app/workflow/graph/multi_dataset_graph/node/analyze_query.py
+++ chat-server/app/workflow/graph/multi_dataset_graph/node/analyze_query.py
@@ -5,7 +5,6 @@ from langchain_core.messages import AIMessage, BaseMessage, ToolMessage
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.runnables import RunnableConfig
 
-from app.core.log import logger
 from app.models.message import ErrorMessage, IntermediateStep
 from app.models.query import QueryResult, ToolUsedResult
 from app.tool_utils.tool_node import has_tool_calls
@@ -57,6 +56,7 @@ async def analyze_query(state: State, config: RunnableConfig) -> dict:
         tools_names = [
             ToolNames.EXECUTE_SQL_QUERY,
             ToolNames.GET_TABLE_SCHEMA,
+            ToolNames.LIST_DATASETS,
             ToolNames.PLAN_SQL_QUERY,
         ]
 
@@ -125,8 +125,6 @@ async def _handle_tool_call_response(
     tool_call_count: int,
 ) -> dict:
     ai_message = response if isinstance(response, BaseMessage) else AIMessage(content=str(response))
-
-    logger.debug(ai_message.content)
 
     return {
         "query_result": query_result,
