4f7f0935d4db93dba3ac73ce72ba80237ef85dec
surajmn1 <suraj.natraj@factly.in>
2025-10-31 12:35:14 +0530

    web : remove hardcoded cors

--- server/interfaces/http/serve.go
+++ server/interfaces/http/serve.go
@@ -45,13 +45,7 @@ func serve(cfg *config.GopieConfig, params *ServerParams, ctx context.Context) e
 		AppName:       "gopie",
 	})
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000,https://gopie.factly.dev,https://*.factly.dev,https://gopie-web.vercel.app",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With, X-CSRF-Token, userID, x-user-id, x-project-ids, x-dataset-ids, x-chat-id, x-organization-id",
-		AllowCredentials: true,
-		MaxAge:           86400,
-	}))
+	app.Use(cors.New(cors.Config{}))
 
 	app.Use(fiberzap.New(fiberzap.Config{
 		Logger: appLogger.Logger,
@@ -167,13 +161,7 @@ func serveInternal(cfg *config.GopieConfig, params *ServerParams, ctx context.Co
 		AppName:       "gopie-internal",
 	})
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000,https://gopie.factly.dev,https://*.factly.dev,https://gopie-web.vercel.app",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With, X-CSRF-Token, userID, x-user-id, x-project-ids, x-dataset-ids, x-chat-id, x-organization-id",
-		AllowCredentials: true,
-		MaxAge:           86400,
-	}))
+	app.Use(cors.New(cors.Config{}))
 
 	app.Use(fiberzap.New(fiberzap.Config{
 		Logger: appLogger.Logger,
@@ -243,13 +231,7 @@ func serveAPI(cfg *config.GopieConfig, params *ServerParams, ctx context.Context
 		AppName:       "gopie-api",
 	})
 
-	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:3000,https://csr.dataful.in",
-		AllowMethods:     "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
-		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Requested-With, X-CSRF-Token, userID, x-user-id, x-project-ids, x-dataset-ids, x-chat-id, x-organization-id",
-		AllowCredentials: true,
-		MaxAge:           86400,
-	}))
+	app.Use(cors.New(cors.Config{}))
 
 	app.Use(fiberzap.New(fiberzap.Config{
 		Logger: appLogger.Logger,
