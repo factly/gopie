1ed28720041a168126753b542dbf2b3056b044f5
elliot14A <akshithkatkuri14@gmail.com>
2025-12-17 17:26:32 +0530

    feat: add zitadel role extraction middleware

--- server/interfaces/http/middleware/role.go
+++ server/interfaces/http/middleware/role.go
@@ -1,15 +1,43 @@
 package middleware
 
 import (
+	"fmt"
+
 	"github.com/factly/gopie/domain/models"
+	"github.com/factly/gopie/infrastructure/zitadel"
 	"github.com/gofiber/fiber/v2"
+	"github.com/spf13/viper"
 )
 
 const RoleCtxKey = "role-ctx-key"
 
 func RoleAuthorization() fiber.Handler {
 	return func(c *fiber.Ctx) error {
-		c.Locals(RoleCtxKey, models.Member)
+		authCtx := zitadel.ZitadelInterceptor.Context(c.Context())
+		orgID := c.Locals(OrganizationCtxKey).(string)
+
+		orgsRole := ""
+
+		claimScope := fmt.Sprintf("urn:zitadel:iam:org:project:%s:roles", viper.GetString("zitadel_project_id"))
+		if claimValue, ok := authCtx.Claims[claimScope].(map[string]any); ok {
+			for role, orgs := range claimValue {
+				if orgsMap, ok := orgs.(map[string]any); ok {
+					for key := range orgsMap {
+						if key == orgID {
+							if orgsRole == "" {
+								orgsRole = role
+							}
+
+							if orgsRole != "" && role != string(models.Member) {
+								orgsRole = role
+							}
+						}
+					}
+				}
+			}
+		}
+
+		c.Locals(RoleCtxKey, models.Role(orgsRole))
 		return c.Next()
 	}
 }
